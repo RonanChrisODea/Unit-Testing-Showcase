5ff752eacfac6cd372b87df7ee0bc06f865ddf1d
RonanChrisODea <55930005+RonanChrisODea@users.noreply.github.com>
2025-02-18 22:06:54 +0000

    Update BankingAppTestRunner.java

--- src/test/java/ie/ronanodea/unitTesting/BankingAppTestRunner.java
+++ src/test/java/ie/ronanodea/unitTesting/BankingAppTestRunner.java
@@ -5,14 +5,6 @@ import org.junit.platform.suite.api.Suite;
 
 /**
  * This test class provides a runnable test suite for this application.  
- * 
- * @see Account
- * @see AccountTest
- * @see AccountManager
- * @see AccountManagerTest
- * @see BankingApp
- * @see BankingAppTest
- * @see BankingAppTestRunner 
  */
 @Suite
 @SelectClasses({AccountTest.class, AccountManagerTest.class, BankingAppTest.class})
