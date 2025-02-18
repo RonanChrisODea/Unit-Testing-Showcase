d237dbbb7eb277d77b7d6e5061d004250c444437
RonanChrisODea <55930005+RonanChrisODea@users.noreply.github.com>
2025-02-18 22:02:06 +0000

    Update AccountManagerTest.java

--- src/test/java/ie/ronanodea/unitTesting/AccountManagerTest.java
+++ src/test/java/ie/ronanodea/unitTesting/AccountManagerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import main.java.ie.ronanodea.unitTesting.AccountManager;
+import ie.ronanodea.unitTesting.AccountManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
