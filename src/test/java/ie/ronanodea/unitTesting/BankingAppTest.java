8f723806a1579a47943fbdb2c649bd5f66ff1c80
RonanChrisODea <55930005+RonanChrisODea@users.noreply.github.com>
2025-02-18 22:06:42 +0000

    Update BankingAppTest.java

--- src/test/java/ie/ronanodea/unitTesting/BankingAppTest.java
+++ src/test/java/ie/ronanodea/unitTesting/BankingAppTest.java
@@ -11,13 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
  * This test class provides a placeholder test to provide coverage for the default banking application 
  * runner. This isn't really required as testing isn't usually applied to main runner methods. 
  * 
- * @see Account
- * @see AccountTest
- * @see AccountManager
- * @see AccountManagerTest
- * @see BankingApp
- * @see BankingAppTest
- * @see BankingAppTestRunner 
  */
 
 class BankingAppTest {
